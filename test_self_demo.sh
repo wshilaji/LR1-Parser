349ca41eba2f248c8b7a16b5d6df627f617ccd5e
duyongsen <duyongsen@bilibili.com>
2024-01-05 11:20:02 +0800

    update

--- test_self_demo.sh
+++ test_self_demo.sh
@@ -1,1 +1,1 @@
-./build/LR1-Parser -g ./test/GrammarExample.yml  -o ./test/LR1-Parse-Table.yml -s ./test/LR1-Parse-Table.csv -a ./test/AST.txt
+./build/LR1-Parser -g ./test/GrammarExample.yml  -t ./test/TokensExample.txt -o ./test/LR1-Parse-Table.yml -s ./test/LR1-Parse-Table.csv -a ./test/AST.txt
